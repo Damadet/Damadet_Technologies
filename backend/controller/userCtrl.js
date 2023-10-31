83c7b4d47fd540cef516185c782d15e544e8dad9
damadet <adedokundamilare41@gmail.com>
2023-10-31 17:46:16 +0100

    Admin login feature

--- backend/controller/userCtrl.js
+++ backend/controller/userCtrl.js
@@ -51,7 +51,41 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
   }
 });
 
+// admin login
+
+const loginAdminCtrl = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  // check if user exists
+  const findUser = await User.findOne({ email });
+  if (findUser.role !== 'admin') throw new Error('Not Authorized');
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    const refreshToken = await generateRefreshToken(findUser?._id);
+    const updateuser = await User.findByIdAndUpdate(
+      findUser.id,
+      {
+        refreshToken,
+      },
+      { new: true },
+    );
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.email,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error('Invalid Credentials');
+  }
+});
+
 module.exports = {
   createUser,
-  loginUserCtrl
+  loginUserCtrl,
+  loginAdminCtrl
 };
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {
-  createUser, loginUserCtrl,
+  createUser, loginUserCtrl, loginAdminCtrl,
 } = require('../controller/userCtrl');
 
 
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
+router.post('/admin-login', loginAdminCtrl);
 
 module.exports = router;
