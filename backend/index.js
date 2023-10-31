a495d83059abbaa7a463a2fcd48365e50a5ac735
damadet <adedokundamilare41@gmail.com>
2023-10-31 16:20:34 +0100

    added database configuration

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const dbConnect = require('./config/dbConnect');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
